8e60fb2044e6f7b8aa897eaa6e68d786e3b5e510
Santiago Paiva <paiva.santiago@gmail.com>
2015-03-21 10:43:53 -0400

    add classes

--- oilsands.py
+++ oilsands.py
@@ -15,4 +15,29 @@ class OilSands(object):
 		self.cost = cost
 
 
+class Model(object):
+
+	def __init__(self,price,wind_power,solar_power):
+		self.price 				= price
+		self.wind_power 	= wind_power
+		self.solar_power 	= solar_power
+
+
+	def create_cost_vector(self,params):
+		return True
+
+class Simulation(object):
+
+	def __init__(self,model):
+		self.model = model
+
+
+class Optimization(object):
+
+	def __init__(self,simulation):
+		self.simulation = simulation
+
+
+
+
 
